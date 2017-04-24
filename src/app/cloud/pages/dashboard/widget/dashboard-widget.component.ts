aa9209109dbf56fda5ed2e9d8f41d8ed8673b31b
vjcspy <mr.vjcspy@gmail.com>
2017-04-24 11:22:09 +0700

    - use chart default style

--- src/app/cloud/pages/dashboard/widget/dashboard-widget.component.ts
+++ src/app/cloud/pages/dashboard/widget/dashboard-widget.component.ts
@@ -41,7 +41,6 @@ export class DashboardWidgetComponent implements OnInit, OnChanges {
   }
   
   protected _prepareChartData() {
-    console.log(this.widgetData.data);
     // prepare lineChart
     if (this._selectedScope) {
       const _scopeChart = _.find(this.widgetData.data, scope => scope['scopeName'] == this._selectedScope);
@@ -96,23 +95,23 @@ export class DashboardWidgetComponent implements OnInit, OnChanges {
           label: this.widgetData.name,
           fill: true,
           lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
+          // backgroundColor: "rgba(75,192,192,0.4)",
+          // borderColor: "rgba(75,192,192,1)",
+          // borderCapStyle: 'butt',
+          // borderDash: [],
+          // borderDashOffset: 0.0,
+          // borderJoinStyle: 'miter',
+          // pointBorderColor: "rgba(75,192,192,1)",
+          // pointBackgroundColor: "#fff",
+          // pointBorderWidth: 1,
+          // pointHoverRadius: 5,
+          // pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          // pointHoverBorderColor: "rgba(220,220,220,1)",
+          // pointHoverBorderWidth: 2,
+          // pointRadius: 1,
+          // pointHitRadius: 10,
           data: this._lineChartData['value'],
-          spanGaps: false,
+          // spanGaps: false,
         }
       ]
     };
@@ -139,27 +138,27 @@ export class DashboardWidgetComponent implements OnInit, OnChanges {
       datasets: [
         {
           label: this.widgetData.name,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 99, 132, 0.2)',
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-            'rgba(55, 159, 164, 1)',
-          ],
-          borderWidth: 1,
+          // backgroundColor: [
+          //   'rgba(255, 99, 132, 0.2)',
+          //   'rgba(54, 162, 235, 0.2)',
+          //   'rgba(255, 206, 86, 0.2)',
+          //   'rgba(75, 192, 192, 0.2)',
+          //   'rgba(153, 102, 255, 0.2)',
+          //   'rgba(255, 159, 64, 0.2)',
+          //   'rgba(255, 99, 132, 0.2)',
+          // ],
+          // borderColor: [
+          //   'rgba(255,99,132,1)',
+          //   'rgba(54, 162, 235, 1)',
+          //   'rgba(255, 206, 86, 1)',
+          //   'rgba(75, 192, 192, 1)',
+          //   'rgba(153, 102, 255, 1)',
+          //   'rgba(255, 159, 64, 1)',
+          //   'rgba(55, 159, 164, 1)',
+          // ],
+          // borderWidth: 1,
           data: this._barChartData['value'],
-          spanGaps: false
+          // spanGaps: false
         }
       ]
     };
