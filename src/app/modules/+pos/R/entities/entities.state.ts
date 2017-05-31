49a4e64bbe57cf590ec3c64eae81f71cd17141e6
vjcspy <mr.vjcspy@gmail.com>
2017-05-31 17:41:51 +0700

    - nothing

--- src/app/modules/+pos/R/entities/entities.state.ts
+++ src/app/modules/+pos/R/entities/entities.state.ts
@@ -49,10 +49,10 @@ export const posEntitiesStateFactory = makeTypedFactory<PosEntitiesState, PosEnt
                               isFinished: false,
                               isDependStore: true,
                               propertyFilter: {
-                                productIds: "",
                                 typeId: "",
                                 visibility: "",
-                                status: 0
+                                status: 0,
+                                productIds: "1,2,3,4,5,9,10,23,2093,51,26,29,32,21,22,24, 67,52,55,58,61,64,53,56,59,62,65,54,57,60,63"
                               },
                               query: "",
                               needRealTime: true,
