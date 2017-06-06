0cfdbbb7bfb071d03b8f013e211369c5c6208af4
vjcspy <mr.vjcspy@gmail.com>
2017-06-06 14:40:30 +0700

    - add integrate with electron

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,9 @@ import {Routes} from "@angular/router";
 import {PageNotFound} from "./pages/404";
 
 export const ROUTES: Routes = [
+  { path: '',
+    redirectTo: '/pos/default/sales/checkout',
+    pathMatch: 'full'
+  },
   {path: '**', component: PageNotFound}
 ];
