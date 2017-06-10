abaa628b5387cdea3e8fd00d6041aa69ba6dac68
vjcspy <mr.vjcspy@gmail.com>
2017-06-10 11:40:47 +0700

    - refactor product helper

--- src/app/modules/+pos/view/R/sales/checkout/popup/product-options.effects.ts
+++ src/app/modules/+pos/view/R/sales/checkout/popup/product-options.effects.ts
@@ -5,13 +5,13 @@ import {PosQuoteActions} from "../../../../../R/quote/quote.actions";
 import {TaxClassDB} from "../../../../../database/xretail/db/tax-class";
 import {List} from "immutable";
 import {Product} from "../../../../../core/framework/catalog/Model/Product";
-import {ProductHelper} from "../../../../../services/helper/product";
 import {ProductOptionsActions} from "./product-options.actions";
+import {ProductHelper} from "../../../../../core/framework/catalog/Helper/Product";
 
 @Injectable()
 export class ProductOptionsEffects {
   
-  constructor(private store$: Store<any>, private actions$: Actions, private productHelper: ProductHelper) { }
+  constructor(private store$: Store<any>, private actions$: Actions) { }
   
   @Effect() retrieveProductDataForPopup = this.actions$.ofType(PosQuoteActions.ACTION_WAIT_GET_PRODUCT_OPTIONS)
                                               .withLatestFrom(this.store$.select('productOptions'))
@@ -20,7 +20,7 @@ export class ProductOptionsEffects {
                                               .map(([action, productOptionsState, entitiesState]) => {
                                                 const taxClass: List<TaxClassDB> = entitiesState[TaxClassDB.getCode()]['items'];
                                                 const product: Product           = productOptionsState['product'];
-                                                product.setTaxClassName(this.productHelper.getProductTaxClass(product.tax_class_id, taxClass));
+                                                product.setTaxClassName(ProductHelper.getProductTaxClass(product.tax_class_id, taxClass));
                                                 
                                                 return {type: ProductOptionsActions.ACTION_RETRIEVE_PRODUCT_INFORMATION, payload: {product}}
     
