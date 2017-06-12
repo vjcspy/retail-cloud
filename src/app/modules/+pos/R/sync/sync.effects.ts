7368a85d059054ce2342a0a3dc25aff4dddb2fdd
vjcspy <mr.vjcspy@gmail.com>
2017-06-12 13:59:17 +0700

    - done sync offline

--- src/app/modules/+pos/R/sync/sync.effects.ts
+++ src/app/modules/+pos/R/sync/sync.effects.ts
@@ -6,11 +6,12 @@ import {PosSyncService} from "./sync.service";
 import {Observable} from "rxjs";
 import * as _ from 'lodash';
 import {NotifyManager} from "../../../../services/notify-manager";
+import {OfflineService} from "../../../share/provider/offline";
 
 @Injectable()
 export class PosSyncEffects {
   
-  constructor(private store$: Store<any>, private actions$: Actions, private posSyncService: PosSyncService, private notify: NotifyManager) { }
+  constructor(private store$: Store<any>, private actions$: Actions, private posSyncService: PosSyncService, private notify: NotifyManager, private offline: OfflineService) { }
   
   @Effect() prepareOrderToSync = this.actions$.ofType(PosSyncActions.ACTION_START_SYNC_CURRENT_ORDER)
                                      .withLatestFrom(this.store$.select('general'))
@@ -26,26 +27,30 @@ export class PosSyncEffects {
                             .withLatestFrom(this.store$.select('quote'),
                                             ([action, generalState], quoteState) => [action, generalState, quoteState])
                             .switchMap(([action, generalState, quoteState]) => {
-                              return this.posSyncService.syncOrderOnline(action.payload['order'], <any>generalState)
-                                         .map((syncData) => {
-                                           const quote   = quoteState.quote;
-                                           const address = quote.getShippingAddress();
-      
-                                           _.forEach(syncData['totals'], (total, key) => {
-                                             if (total != null)
-                                               address.setData(key, total);
-                                           });
-                                           if (syncData.hasOwnProperty("reward_point") && _.isObject(syncData["reward_point"])) {
-                                             quote.setData('reward_point', syncData["reward_point"]);
-                                           } else {
-                                             quote.unsetData("reward_point");
-                                           }
-                                           if (syncData['totals']['coupon_code']) {
-                                             this.notify.success('coupon_code_accepted');
-                                           }
-      
-                                           return {type: PosSyncActions.ACTION_SYNC_ORDER_SUCCESS, payload: {quote: quote}};
-                                         })
-                                         .catch((e) => Observable.of({type: PosSyncActions.ACTION_SYNC_ORDER_ERROR, payload: {e}}));
+                              const quote = quoteState.quote;
+                              if (this.offline.online) {
+                                return this.posSyncService.syncOrderOnline(action.payload['order'], <any>generalState)
+                                           .map((syncData) => {
+                                             const address = quote.getShippingAddress();
+        
+                                             _.forEach(syncData['totals'], (total, key) => {
+                                               if (total != null)
+                                                 address.setData(key, total);
+                                             });
+                                             if (syncData.hasOwnProperty("reward_point") && _.isObject(syncData["reward_point"])) {
+                                               quote.setData('reward_point', syncData["reward_point"]);
+                                             } else {
+                                               quote.unsetData("reward_point");
+                                             }
+                                             if (syncData['totals']['coupon_code']) {
+                                               this.notify.success('coupon_code_accepted');
+                                             }
+        
+                                             return {type: PosSyncActions.ACTION_SYNC_ORDER_SUCCESS, payload: {quote: quote}};
+                                           })
+                                           .catch((e) => Observable.of(<any>{type: PosSyncActions.ACTION_SYNC_ORDER_ERROR, payload: {e}}));
+                              } else {
+                                return Observable.of({type: PosSyncActions.ACTION_SYNC_ORDER_SUCCESS, payload: {quote: quote}});
+                              }
                             });
 }
