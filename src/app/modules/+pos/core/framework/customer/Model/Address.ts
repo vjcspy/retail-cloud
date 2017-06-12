79ec90986d0f983ffa9e6c9a887a56b7cd456256
vjcspy <mr.vjcspy@gmail.com>
2017-06-12 11:15:44 +0700

    - add function to get Address JS Data

--- src/app/modules/+pos/core/framework/customer/Model/Address.ts
+++ src/app/modules/+pos/core/framework/customer/Model/Address.ts
@@ -1,10 +1,26 @@
 import {DataObject} from "../../General/DataObject";
 export class Address extends DataObject {
-    getCountryId(): number {
-        return this.getData('country_id');
+  getCountryId(): number {
+    return this.getData('country_id');
+  }
+  
+  getPostcode(): string {
+    return this.getData('postcode');
+  }
+  
+  toJS() {
+    return {
+      city: this.getData('city'),
+      company: this.getData('company'),
+      country_id: this.getData('country_id'),
+      first_name: this.getData('first_name'),
+      last_name: this.getData('last_name'),
+      middlename: this.getData('middlename'),
+      postcode: this.getData('postcode'),
+      region: this.getData('region'),
+      region_id: this.getData('region_id'),
+      street: this.getData('street'),
+      telephone: this.getData('telephone'),
     }
-
-    getPostcode(): string {
-        return this.getData('postcode');
-    }
+  }
 }
