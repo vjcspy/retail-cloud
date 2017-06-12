b239151ecafda37937520deb878f97f4fb11d037
vjcspy <mr.vjcspy@gmail.com>
2017-06-12 09:44:34 +0700

    - nothing

--- src/app/modules/+pos/R/general/general.state.ts
+++ src/app/modules/+pos/R/general/general.state.ts
@@ -11,8 +11,8 @@ export interface PosGeneralState {
 export interface PosGeneralStateRecord extends TypedRecord<PosGeneralStateRecord>, PosGeneralState {}
 
 export const posGeneralStateFactory = makeTypedFactory<PosGeneralState, PosGeneralStateRecord>({
-                                                                                                 baseUrl: "https://magento2demo.connectpos.com",
-                                                                                                 // baseUrl: "http://mage2.dev",
+                                                                                                 // baseUrl: "https://magento2demo.connectpos.com",
+                                                                                                 baseUrl: "http://mage2.dev",
                                                                                                  store: {},
                                                                                                  register: {},
                                                                                                  warehouse: {},
