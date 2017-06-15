3d882fd71b10cc58c7f7f13b8f85a0c9a3a4401c
vjcspy <mr.vjcspy@gmail.com>
2017-06-15 23:20:20 +0700

    - revert

--- src/app/modules/+pos/view/R/sales/checkout/step/step.reducer.ts
+++ src/app/modules/+pos/view/R/sales/checkout/step/step.reducer.ts
@@ -36,7 +36,7 @@ export const posStepReducer: ActionReducer<PosStepStateRecord> = (state: PosStep
     
     case PosStepActions.ACTION_ADD_PAYMENT_METHOD_TO_ORDER:
       return state.update('paymentMethodUsed', (l) => l.map((_p) => {
-        return Object.assign({}, {..._p}, {isChanging: true});
+        return Object.assign({}, {..._p}, {isChanging: false});
       }).push(action.payload['payment']));
     
     case PosStepActions.ACTION_CHANGE_AMOUNT_PAYMENT:
