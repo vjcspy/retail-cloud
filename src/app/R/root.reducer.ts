97a2c1d4fc4fb1e4a9ebf2ba6b1d2da544f415dd
vjcspy <mr.vjcspy@gmail.com>
2017-06-16 23:38:13 +0700

    - nothing

--- src/app/R/root.reducer.ts
+++ src/app/R/root.reducer.ts
@@ -1,6 +1,5 @@
 import {Action, ActionReducer} from "@ngrx/store";
 import {RootState, rootStateFactory, RootStateRecord} from "./root.state";
-import {Record} from "immutable";
 
 export const rootReducer: ActionReducer<RootState> = (state: RootStateRecord = rootStateFactory(), action: Action) => {
   switch (action.type) {
