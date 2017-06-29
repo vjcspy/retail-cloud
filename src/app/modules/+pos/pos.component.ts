da99ab00d204e6431013aef6ee4337072b9e7a88
vjcspy <mr.vjcspy@gmail.com>
2017-06-29 15:39:34 +0700

    - remove unnecessary service

--- src/app/modules/+pos/pos.component.ts
+++ src/app/modules/+pos/pos.component.ts
@@ -1,6 +1,4 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {Store} from "@ngrx/store";
-import {posReducer, PosState} from "./R/index";
 import {OfflineService} from "../share/provider/offline";
 import {RetailTranslate} from "../../services/retail-translate";
 import {TranslateService} from "@ngx-translate/core";
@@ -13,7 +11,7 @@ import {TranslateService} from "@ngx-translate/core";
              changeDetection: ChangeDetectionStrategy.OnPush
            })
 export class PosComponent {
-  constructor(private store: Store<PosState>, private offline: OfflineService, private retailTranslate: RetailTranslate, private translate: TranslateService) {
+  constructor(private offline: OfflineService, private retailTranslate: RetailTranslate, private translate: TranslateService) {
     this.offline.init();
     
     this.translate.setDefaultLang('en');
