b0d8fdc93c95c7def2dfdb66d370bf1c72682871
vjcspy <mr.vjcspy@gmail.com>
2017-07-05 10:54:55 +0700

    - done customer behavior add/minus

--- src/app/modules/+pos/view/default/sales/checkout/cart/items.component.ts
+++ src/app/modules/+pos/view/default/sales/checkout/cart/items.component.ts
@@ -10,6 +10,8 @@ import {CartItemActions} from "../../../../R/sales/checkout/cart/item.actions";
 import {PosQuoteActions} from "../../../../../R/quote/quote.actions";
 import {QuoteItemActions} from "../../../../../R/quote/item/item.actions";
 import {NotifyManager} from "../../../../../../../services/notify-manager";
+import {CartCustomerActions} from "../../../../R/sales/checkout/cart/customer.actions";
+import {Customer} from "../../../../../core/framework/customer/Model/Customer";
 
 @Component({
              // moduleId: module.id,
@@ -23,11 +25,14 @@ export class PosDefaultSalesCheckoutCartItemsComponent {
   @Input() cartItemState: CartItemState;
   @Input() cartCustomerState: CartCustomerState;
   
+  protected customerInfo;
+  
   constructor(protected cartItemActions: CartItemActions,
               protected numberHelper: NumberHelper,
               protected posQuoteActions: PosQuoteActions,
               protected quoteItemActions: QuoteItemActions,
-              protected notify: NotifyManager) { }
+              protected notify: NotifyManager,
+              protected cartCustomerActions: CartCustomerActions) { }
   
   toggleItemInCart(event, i): void {
     if (_.indexOf(["product-name", "c-num", "cart-head", "c-name", "old-pr", "cart-row", "c-price", "p-product-name", "regular-pr"],
@@ -73,5 +78,41 @@ export class PosDefaultSalesCheckoutCartItemsComponent {
     else {
       return '/assets/img/no-image.jpg';
     }
+  }
+  
+  customerAction($event, iconAction: boolean = false) {
+    const customer = this.quoteState.customer;
+    
+    if (iconAction === false && $event.target.className.indexOf('customer-icon-action') === -1) {
+      if (customer && customer['id'] && customer.getId() !== this.configState.setting.customer.getDefaultCustomerId()) {
+        // show popup
+      } else {
+        this.cartCustomerActions.updateActionCartState('inSearchCustomers', true);
+      }
+    } else if (iconAction === true && $event.target.className.indexOf('customer-icon-action') > -1) {
+      if (customer && customer['id'] && customer.getId() !== this.configState.setting.customer.getDefaultCustomerId()) {
+        // remove customer
+        this.posQuoteActions.setCustomerToQuote(new Customer(), true);
+      } else {
+        this.cartCustomerActions.updateActionCartState('inSearchCustomers', true);
+      }
+    }
+  }
+  
+  getCustomerInfo(force: boolean = false) {
+    if (typeof this.customerInfo === 'undefined' || force) {
+      const customer = this.quoteState.customer;
+      let customerName, customerEmail;
+      if (customer && customer['id']) {
+        customerName  = customer['first_name'] + " " + customer['last_name'];
+        customerEmail = customer['email'];
+      } else {
+        customerName  = 'Guest';
+        customerEmail = '';
+      }
+      
+      this.customerInfo = {customerName, customerEmail};
+    }
+    return this.customerInfo;
   }
 }
