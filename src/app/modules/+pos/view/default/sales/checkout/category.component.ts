a9958cf8aff6ab7182ed7fc214e22c3cb128c3c9
huongtt <huongtt@smartosc.com>
2017-07-05 09:55:26 +0700

    Update frontend (#58)
    * Update
    
    * Fixbug Checkout
    
    * update style
    
    * Fixbug tablet
    
    * update style for Invoice print
    
    * update icon - category
    
    * update style for home
    
    * update style for gridview
    
    * update left menu
    
    * update style for menu
    
    * update add to cart for grid mode
    
    * update listing
    
    * update cart
    
    * Edit style
    
    * update category slider
    
    * update category slider
    
    * update category listing and customer
    
    * update icon sale

--- src/app/modules/+pos/view/default/sales/checkout/category.component.ts
+++ src/app/modules/+pos/view/default/sales/checkout/category.component.ts
@@ -20,37 +20,43 @@ export class PosDefaultSalesCheckoutCategoryComponent implements OnInit, AfterVi
   ngOnInit() { }
   
   ngAfterViewInit(): void {
-    // setTimeout(() => {
-    //   jQuery(".slider")['slick']({
-    //
-    //                                // normal options...
-    //                                infinite: false,
-    //
-    //                                // the magic
-    //                                responsive: [{
-    //
-    //                                  breakpoint: 1024,
-    //                                  settings: {
-    //                                    slidesToShow: 3,
-    //                                    infinite: true
-    //                                  }
-    //
-    //                                }, {
-    //
-    //                                  breakpoint: 600,
-    //                                  settings: {
-    //                                    slidesToShow: 2,
-    //                                    dots: true
-    //                                  }
-    //
-    //                                }, {
-    //
-    //                                  breakpoint: 300,
-    //                                  settings: "unslick" // destroys slick
-    //
-    //                                }]
-    //                              });
-    // }, 1000);
+  setTimeout(() => {
+  jQuery(".cate-slider")['slick']({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  });
+  }, 1000);
+
   }
   
   private updateCategoryMode(isCategoryMode: boolean = true) {
