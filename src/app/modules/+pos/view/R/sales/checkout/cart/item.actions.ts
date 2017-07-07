e7297f45cd6fda792799c6df5b8881de25b71480
vjcspy <mr.vjcspy@gmail.com>
2017-07-07 15:42:38 +0700

    - update cart item style when toggle block total

--- src/app/modules/+pos/view/R/sales/checkout/cart/item.actions.ts
+++ src/app/modules/+pos/view/R/sales/checkout/cart/item.actions.ts
@@ -1,13 +1,26 @@
 import {Injectable} from '@angular/core';
-import {Store} from "@ngrx/store";
+import {Action, Store} from "@ngrx/store";
 
 @Injectable()
 export class CartItemActions {
-  static ACTION_CHANGE_ROW_SELECTED = 'ACTION_CHANGE_ROW_SELECTED';
   
   constructor(private store$: Store<any>) { }
   
+  static ACTION_CHANGE_ROW_SELECTED = 'ACTION_CHANGE_ROW_SELECTED';
+  
   changeRowSelected(cartItemRowSelected: number) {
     this.store$.dispatch({type: CartItemActions.ACTION_CHANGE_ROW_SELECTED, payload: {cartItemRowSelected}});
+  }
+  
+  static ACTION_UPDATE_CART_ITEM_STYLES = 'ACTION_UPDATE_CART_ITEM_STYLES';
+  
+  updateCartItemStyles(cartItemsStyle, dispatch: boolean = true): Action {
+    const action = {type: CartItemActions.ACTION_UPDATE_CART_ITEM_STYLES, payload: {cartItemsStyle}};
+    
+    if (dispatch === true) {
+      this.store$.dispatch(action);
+    }
+    
+    return action;
   }
 }
--- src/app/modules/+pos/view/R/sales/checkout/cart/item.effects.ts
+++ src/app/modules/+pos/view/R/sales/checkout/cart/item.effects.ts
@@ -1,8 +1,28 @@
 import {Injectable} from '@angular/core';
+import {Store} from "@ngrx/store";
+import {Actions, Effect} from "@ngrx/effects";
+import {CartTotalsActions} from "./totals.actions";
+import {PosQuoteActions} from "../../../../../R/quote/quote.actions";
+import {CartItemActions} from "./item.actions";
 
 @Injectable()
 export class CartItemEffects {
   
-  constructor() { }
+  constructor(private store$: Store<any>, private actions$: Actions,
+              private cartItemsActions: CartItemActions) { }
   
+  @Effect() recalculateCartStyle = this.actions$
+                                       .ofType(
+                                         CartTotalsActions.ACTION_TOGGLE_BLOCK_TOTAL_STATE,
+                                         PosQuoteActions.ACTION_RESOLVE_QUOTE,
+                                         PosQuoteActions.ACTION_CLEAR_QUOTE,
+                                       )
+                                       .debounceTime(500)
+                                       .map(() => {
+                                         let cartTotalHeight = jQuery('#cart-totals-comp').height();
+                                         if (isNaN(cartTotalHeight)) {
+                                           cartTotalHeight = 0;
+                                         }
+                                         return this.cartItemsActions.updateCartItemStyles({height: `calc(100% - ${cartTotalHeight}px)`}, false);
+                                       });
 }
