72b3b51a4ad36373c73afe8e6a77968640c3f1e5
vjcspy <mr.vjcspy@gmail.com>
2017-07-07 14:47:24 +0700

    - refactor html

--- src/app/modules/+pos/view/default/sales/checkout/cart.component.ts
+++ src/app/modules/+pos/view/default/sales/checkout/cart.component.ts
@@ -4,6 +4,7 @@ import {CartCustomerState} from "../../../R/sales/checkout/cart/customer.state";
 import {PosConfigState} from "../../../../R/config/config.state";
 import {CartItemState} from "../../../R/sales/checkout/cart/item.state";
 import {CartTotalsState} from "../../../R/sales/checkout/cart/totals.state";
+import {RouterActions} from "../../../../../../R/router/router.actions";
 
 @Component({
              // moduleId: module.id,
@@ -18,12 +19,16 @@ export class PosDefaultSalesCheckoutCartComponent implements OnInit {
   @Input() cartItemState: CartItemState;
   @Input() cartTotalsState: CartTotalsState;
   
-  constructor() { }
+  constructor(private routerActions: RouterActions) { }
   
   ngOnInit() {
   }
   
   protected onResizeCart() {
     console.log('not implement resize cart');
+  }
+  
+  openShift() {
+    this.routerActions.go('pos/default/sales/shifts');
   }
 }
