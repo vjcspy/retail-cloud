2f1e67875d6e8f406e83d68f82f33edec302500a
vjcspy <mr.vjcspy@gmail.com>
2017-07-10 17:08:24 +0700

    - nothing

--- src/app/modules/+pos/view/R/sales/receipts/receipt.service.ts
+++ src/app/modules/+pos/view/R/sales/receipts/receipt.service.ts
@@ -1,18 +1,14 @@
 import {Injectable} from '@angular/core';
 import {Subject} from "rxjs";
 import {RequestService} from "../../../../../../services/request";
-import {NotifyManager} from "../../../../../../services/notify-manager";
-import {GeneralMessage} from "../../../../services/general/message";
 import {ApiManager} from "../../../../../../services/api-manager";
 import {PosGeneralState} from "../../../../R/general/general.state";
 
 @Injectable()
 export class ReceiptService {
-  
   protected receiptSubject = new Subject();
   
-  constructor(private request: RequestService, private notify: NotifyManager, private apiManager: ApiManager) { }
-  
+  constructor(private request: RequestService, private apiManager: ApiManager) { }
   
   printReceipt() {
     this.receiptSubject.next();
