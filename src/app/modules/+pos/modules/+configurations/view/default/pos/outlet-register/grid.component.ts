c47689fb17133f68a71c8c74f74c6c380ae19bd6
vjcspy <mr.vjcspy@gmail.com>
2017-07-12 10:21:17 +0700

    - save outlet data

--- src/app/modules/+pos/modules/+configurations/view/default/pos/outlet-register/grid.component.ts
+++ src/app/modules/+pos/modules/+configurations/view/default/pos/outlet-register/grid.component.ts
@@ -2,6 +2,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {Observable} from "rxjs/Observable";
 import {Store} from "@ngrx/store";
 import {ConfigurationsState} from "../../../../R/index";
+import {RouterActions} from "../../../../../../../../R/router/router.actions";
 
 @Component({
              // moduleId: module.id,
@@ -13,7 +14,12 @@ import {ConfigurationsState} from "../../../../R/index";
 export class ConfigurationsDefaultPosOutletRegisterGridComponent {
   configurations$: Observable<ConfigurationsState>;
   
-  constructor(private store$: Store<any>) {
+  constructor(private store$: Store<any>,
+              private routerActions: RouterActions) {
     this.configurations$ = this.store$.select('configurations');
+  }
+  
+  newOutlet() {
+    this.routerActions.go('pos/configurations/default/pos/outlet/edit', 0);
   }
 }
