9db0c374ce34f52f001e2f3be5605101d4e0386c
vjcspy <mr.vjcspy@gmail.com>
2017-07-20 10:55:34 +0700

    - add note to order ok

--- src/app/modules/+pos/view/R/sales/checkout/cart/action-bar.actions.ts
+++ src/app/modules/+pos/view/R/sales/checkout/cart/action-bar.actions.ts
@@ -5,12 +5,6 @@ import {Action, Store} from "@ngrx/store";
 export class CartActionBarActions {
   constructor(private store$: Store<any>) { }
   
-  static CHANGE_STATE_NOTE_POPUP = 'CHANGE_STATE_NOTE_POPUP';
-  
-  changeStateNotePopup(state: boolean): void {
-    this.store$.dispatch({type: CartActionBarActions.CHANGE_STATE_NOTE_POPUP, payload: {state}})
-  }
-  
   static ACTION_CHANGE_MODE_ACTIONS_POPUP = 'ACTION_CHANGE_MODE_ACTIONS_POPUP';
   
   changeModeActionPopup(isOpenActions, dispatch: boolean = true): Action {
@@ -23,10 +17,10 @@ export class CartActionBarActions {
     return action;
   }
   
-  static ACTION_CHANGE_MODE_ACTIONS_ORDER_ONHOLD_POPUP = 'ACTION_CHANGE_MODE_ACTIONS_ORDER_ONHOLD_POPUP';
+  static ACTION_CHANGE_MODE_POPUP = 'ACTION_CHANGE_MODE_POPUP';
   
-  changeModeOrderOnholdPopup(isOpenOrderOnhold, dispatch: boolean = true): Action {
-    const action = {type: CartActionBarActions.ACTION_CHANGE_MODE_ACTIONS_ORDER_ONHOLD_POPUP, payload: {isOpenOrderOnhold}};
+  changeModePopup(isOpeningPopup, dispatch: boolean = true): Action {
+    const action = {type: CartActionBarActions.ACTION_CHANGE_MODE_POPUP, payload: {isOpeningPopup}};
     
     if (dispatch === true) {
       this.store$.dispatch(action);
--- src/app/modules/+pos/view/default/sales/checkout/actions-bar/note.component.ts
+++ src/app/modules/+pos/view/default/sales/checkout/actions-bar/note.component.ts
@@ -1,8 +1,9 @@
-import {ChangeDetectionStrategy, Component, ElementRef, HostListener, Input, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, HostListener, Input, OnInit, ViewChild} from '@angular/core';
 import {CartActionBarState} from "../../../../R/sales/checkout/cart/action-bar.state";
 import {PosQuoteState} from "../../../../../R/quote/quote.state";
 import {PosQuoteActions} from "../../../../../R/quote/quote.actions";
 import {CartActionBarActions} from "../../../../R/sales/checkout/cart/action-bar.actions";
+import {NotifyManager} from "../../../../../../../services/notify-manager";
 
 @Component({
              // moduleId: module.id,
@@ -10,24 +11,23 @@ import {CartActionBarActions} from "../../../../R/sales/checkout/cart/action-bar
              templateUrl: 'note.component.html',
              changeDetection: ChangeDetectionStrategy.OnPush
            })
-export class PosDefaultSalesCheckoutActionsBarNoteComponent {
+export class PosDefaultSalesCheckoutActionsBarNoteComponent implements OnInit {
   @Input() cartActionBarState: CartActionBarState;
   @Input() quoteState: PosQuoteState;
-  @ViewChild('noteElem') noteElem: ElementRef;
+  public note: string;
   
-  constructor(protected cartActionBarActions: CartActionBarActions, protected quoteActions: PosQuoteActions) { }
+  constructor(public cartActionBarActions: CartActionBarActions,
+              protected quoteActions: PosQuoteActions,
+              private notify: NotifyManager) { }
   
-  @HostListener('document:click', ['$event.target']) onClick(target) {
-    if (target.className.indexOf('icon-edit') > -1 || target.className.indexOf('snote-edit') > -1) {
-    }
-    else if (this.noteElem && !this.noteElem.nativeElement.contains(target)) {
-      this.cartActionBarActions.changeStateNotePopup(false);
-    }
+  ngOnInit(): void {
+    console.log(this.quoteState.quote.getData('retail_note'));
+    this.note = this.quoteState.quote.getData('retail_note');
   }
   
-  protected toggleNote(event) {
-    if (event.target.className.indexOf('icon-edit') > -1 || event.target.className.indexOf('snote-edit') > -1) {
-      this.cartActionBarActions.changeStateNotePopup(!this.cartActionBarState.isOpeningNote)
-    }
+  saveNote() {
+    this.quoteState.quote.setData('retail_note', this.note);
+    this.cartActionBarActions.changeModePopup(null);
+    this.notify.success("save_note_successfully");
   }
 }
