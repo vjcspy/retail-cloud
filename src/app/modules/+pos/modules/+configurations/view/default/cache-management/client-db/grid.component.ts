f189d511f95d4c1b73b8d903eb91e0ddb9bbcad4
vjcspy <mr.vjcspy@gmail.com>
2017-07-26 10:31:01 +0700

    - add flush cache and flush storage button

--- src/app/modules/+pos/modules/+configurations/view/default/cache-management/client-db/grid.component.ts
+++ src/app/modules/+pos/modules/+configurations/view/default/cache-management/client-db/grid.component.ts
@@ -1,6 +1,9 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {ConfigurationsClientDbState} from "../../../../R/cache/client-db/client-db.state";
 import {ConfigurationsClientDbActions} from "../../../../R/cache/client-db/client-db.actions";
+import {DatabaseManager} from "../../../../../../../../services/database-manager";
+import {RetailDB} from "../../../../../../database/xretail/db/retail-db";
+import {AppStorage} from "../../../../../../../../services/storage";
 
 @Component({
              // moduleId: module.id,
@@ -10,14 +13,25 @@ import {ConfigurationsClientDbActions} from "../../../../R/cache/client-db/clien
              styleUrls: ['grid.component.scss']
            })
 
-export class ConfigurationsDefaultCacheManagementClientDBGridComponent implements OnInit {
+export class ConfigurationsDefaultCacheManagementClientDBGridComponent {
   @Input() clientDbState: ConfigurationsClientDbState;
   
-  constructor(public clientDbActions: ConfigurationsClientDbActions) { }
-  
-  ngOnInit() { }
+  constructor(public clientDbActions: ConfigurationsClientDbActions,
+              protected databaseManager: DatabaseManager,
+              protected storage: AppStorage) { }
   
   getDateTimeString(time) {
     return new Date(time);
+  }
+  
+  async flushAll() {
+    let db: RetailDB = this.databaseManager.getDbInstance();
+    await db.delete();
+    location.reload(true);
+  }
+  
+  flushStorage() {
+    this.storage.localClear();
+    location.reload(true);
   }
 }
