5c9f6ba2d3ec2f8015645382a9594bcd7aecd566
vjcspy <mr.vjcspy@gmail.com>
2017-07-31 14:32:07 +0700

    - pull customer base on setting

--- src/app/modules/+pos/view/R/router/router-effects.ts
+++ src/app/modules/+pos/view/R/router/router-effects.ts
@@ -3,6 +3,7 @@ import {Action, Store} from "@ngrx/store";
 import {Actions, Effect} from "@ngrx/effects";
 import {routerActions} from "@ngrx/router-store";
 import {PosPullActions} from "../../../R/entities/pull.actions";
+import {PosConfigState} from "../../../R/config/config.state";
 
 @Injectable()
 export class PosViewRouterEffects {
@@ -12,24 +13,29 @@ export class PosViewRouterEffects {
   @Effect() whenGoCheckout = this.actions$
                                  .ofType(routerActions.UPDATE_LOCATION)
                                  .filter((action: Action) => action.payload['path'] === '/pos/default/sales/checkout')
-                                 .map(() => {
-                                   return this.pullActions.pullEntities([
-                                                                          'retailConfig',
-                                                                          'settings',
-                                                                          'countries',
-                                                                          'taxClass',
-                                                                          'taxes',
-                                                                          'receipts',
-                                                                          'payment',
-                                                                          'userOrderCount',
-                                                                          // 'warehouse',
-                                                                          // 'permission',
-                                                                          'customerGroup',
-                                                                          'customers',
-                                                                          'category',
-                                                                          'products',
-                                                                          'orders',
-                                                                        ], false);
+                                 .withLatestFrom(this.store$.select('config'))
+                                 .map((z: any) => {
+                                   const configState: PosConfigState = z[1];
+                                   let entityPull                    = [
+                                     'retailConfig',
+                                     'settings',
+                                     'countries',
+                                     'taxClass',
+                                     'taxes',
+                                     'receipts',
+                                     'payment',
+                                     'userOrderCount',
+                                     // 'warehouse',
+                                     // 'permission',
+                                     'customerGroup',
+                                     'category',
+                                     'products',
+                                     'orders',
+                                   ];
+                                   if (!configState.posRetailConfig.useCustomerOnlineMode) {
+                                     entityPull.push('customers');
+                                   }
+                                   return this.pullActions.pullEntities(entityPull, false);
                                  });
   
   @Effect() whenGoOrders = this.actions$
