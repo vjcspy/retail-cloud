6ffd07daa678d9171de7eecac2861bfa0f8ba23a
vjcspy <mr.vjcspy@gmail.com>
2017-08-05 11:19:16 +0700

    - chua init xong setting thi luon nhan la use customer online

--- src/app/modules/+pos/view/R/router/router-effects.ts
+++ src/app/modules/+pos/view/R/router/router-effects.ts
@@ -3,7 +3,6 @@ import {Action, Store} from "@ngrx/store";
 import {Actions, Effect} from "@ngrx/effects";
 import {routerActions} from "@ngrx/router-store";
 import {PosPullActions} from "../../../R/entities/pull.actions";
-import {PosConfigState} from "../../../R/config/config.state";
 
 @Injectable()
 export class PosViewRouterEffects {
@@ -11,31 +10,27 @@ export class PosViewRouterEffects {
   constructor(private store$: Store<any>, private actions$: Actions, private pullActions: PosPullActions) { }
   
   @Effect() whenGoCheckout = this.actions$
-                                 .ofType(routerActions.UPDATE_LOCATION)
+                                 .ofType(
+                                   routerActions.UPDATE_LOCATION
+                                 )
                                  .filter((action: Action) => action.payload['path'] === '/pos/default/sales/checkout')
-                                 .withLatestFrom(this.store$.select('config'))
                                  .map((z: any) => {
-                                   const configState: PosConfigState = z[1];
-                                   let entityPull                    = [
-                                     'retailConfig',
-                                     'settings',
-                                     'countries',
-                                     'taxClass',
-                                     'taxes',
-                                     'receipts',
-                                     'payment',
-                                     'userOrderCount',
-                                     // 'warehouse',
-                                     // 'permission',
-                                     'customerGroup',
-                                     'category',
-                                     'products',
-                                     'orders',
-                                   ];
-                                   if (!configState.posRetailConfig.useCustomerOnlineMode) {
-                                     entityPull.push('customers');
-                                   }
-                                   return this.pullActions.pullEntities(entityPull, false);
+                                   return this.pullActions.pullEntities([
+                                                                          'retailConfig',
+                                                                          'settings',
+                                                                          'countries',
+                                                                          'taxClass',
+                                                                          'taxes',
+                                                                          'receipts',
+                                                                          'payment',
+                                                                          'userOrderCount',
+                                                                          // 'warehouse',
+                                                                          // 'permission',
+                                                                          'customerGroup',
+                                                                          'category',
+                                                                          'products',
+                                                                          'orders',
+                                                                        ], false);
                                  });
   
   @Effect() whenGoOrders = this.actions$
