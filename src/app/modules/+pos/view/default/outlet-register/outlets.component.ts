69f634c33fc52519188d984755346a9cdcb32851
vjcspy <mr.vjcspy@gmail.com>
2017-08-14 10:27:02 +0700

    - refactor

--- src/app/modules/+pos/view/default/outlet-register/outlets.component.ts
+++ src/app/modules/+pos/view/default/outlet-register/outlets.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {PosEntitiesState} from "../../../R/entities/entities.state";
 import {PosGeneralActions} from "../../../R/general/general.actions";
 import {PosPullState} from "../../../R/entities/pull.state";
@@ -7,7 +7,8 @@ import {NotifyManager} from "../../../../../services/notify-manager";
 @Component({
              // moduleId: module.id,
              selector: 'pos-default-outlet-register-outlets',
-             templateUrl: 'outlets.component.html'
+             templateUrl: 'outlets.component.html',
+             changeDetection: ChangeDetectionStrategy.OnPush
            })
 export class PosDefaultOutletRegisterOutletsComponent implements OnInit {
   @Input() entitiesState: PosEntitiesState;
@@ -19,7 +20,9 @@ export class PosDefaultOutletRegisterOutletsComponent implements OnInit {
   
   selectOutletAndRegister(outletId: number, registerId: number): void {
     if (this.pullState.isPullingChain) {
-      this.notify.info('wait_until_data_pull_successfully')
+      this.notify.info('wait_until_data_pull_successfully');
+      
+      return;
     } else {
       this.generalActions.selectOutletRegister(outletId, registerId);
     }
--- src/app/modules/+pos/view/default/outlet-register/website.component.ts
+++ src/app/modules/+pos/view/default/outlet-register/website.component.ts
@@ -1,23 +1,30 @@
-import {AfterViewInit, Component, Input, OnChanges, SimpleChange, SimpleChanges} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChange, SimpleChanges} from '@angular/core';
 import {PosGeneralState} from "../../../R/general/general.state";
 import {AccountActions} from "../../../../../R/account/account.actions";
 import {LocalStorage} from "ngx-webstorage";
 import * as _ from 'lodash';
 import {PosGeneralActions} from "../../../R/general/general.actions";
 import {RouterActions} from "../../../../../R/router/router.actions";
+import {PosPullState} from "../../../R/entities/pull.state";
+import {NotifyManager} from "../../../../../services/notify-manager";
 
 @Component({
              // moduleId: module.id,
              selector: 'pos-default-outlet-register-website',
-             templateUrl: 'website.component.html'
+             templateUrl: 'website.component.html',
+             changeDetection: ChangeDetectionStrategy.OnPush
            })
 export class PosDefaultOutletRegisterWebsiteComponent implements OnChanges, AfterViewInit {
   @Input() generalState: PosGeneralState;
+  @Input() pullState: PosPullState;
   
   @LocalStorage('baseUrl')
   public baseUrl: string;
   
-  constructor(public accountActions: AccountActions, public generalActions: PosGeneralActions, private routerActions: RouterActions) { }
+  constructor(public accountActions: AccountActions,
+              protected notify: NotifyManager,
+              public generalActions: PosGeneralActions,
+              protected routerActions: RouterActions) { }
   
   ngOnChanges(changes: SimpleChanges): void {
     if (!this.baseUrl || this.baseUrl === "") {
@@ -42,9 +49,17 @@ export class PosDefaultOutletRegisterWebsiteComponent implements OnChanges, Afte
   }
   
   selectWebsite($event) {
+    if (this.pullState.isPullingChain) {
+      this.notify.info('wait_until_data_pull_successfully');
+      
+      return;
+    }
+    
     if (_.isString($event) && $event !== 'null' && this.baseUrl !== $event) {
       this.baseUrl = $event;
       this.generalActions.selectWebsite(this.baseUrl);
+    } else {
+      this.notify.error("sorry_we_can_not_select_base_url");
     }
   }
   
