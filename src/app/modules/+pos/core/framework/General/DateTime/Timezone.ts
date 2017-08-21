eaf74380b852f1a4bb316056fe2fbfab785e14f2
vjcspy <mr.vjcspy@gmail.com>
2017-08-21 10:16:57 +0700

    - refactor

--- src/app/modules/+pos/core/framework/General/DateTime/Timezone.ts
+++ src/app/modules/+pos/core/framework/General/DateTime/Timezone.ts
@@ -1,34 +1,34 @@
 import * as moment from "moment";
 import {SettingDB} from "../../../../database/xretail/db/setting";
 export class Timezone {
-    isScopeDateInInterval(scope, dateFrom = null, dateTo = null): boolean {
-        let _currentMoment = moment();
-
-        let result = false;
-        if (!!dateFrom && moment(dateFrom).isAfter(_currentMoment)) {
-
-        }
-        else if (!!dateTo) {
-            let dateToAfterAddOneDay = moment(dateTo).add(86400, "s");
-            if (dateToAfterAddOneDay.isAfter(_currentMoment))
-                result = true;
-        }
-        else
-            result = true;
-
-        return result;
+  isScopeDateInInterval(scope, dateFrom = null, dateTo = null): boolean {
+    let _currentMoment = moment();
+    
+    let result = false;
+    if (!!dateFrom && moment(dateFrom).isAfter(_currentMoment)) {
+    
     }
-
-    static getCurrentStringTime(data :boolean = false): string {
-        if (data == true){
-            if (SettingDB.getStoreConfigGroup("store")['time_zone'] < 0) {
-                return moment().utc().add(SettingDB.getStoreConfigGroup("store")['time_zone'], "s").format("YYYY-MM-DD HH:mm:s");
-            } else {
-                return moment().utc().subtract(SettingDB.getStoreConfigGroup("store")['time_zone'], "s").format("YYYY-MM-DD HH:mm:s");
-            }
-        }else{
-            return moment().format("YYYY-MM-DD HH:mm:s");
-        }
+    else if (!!dateTo) {
+      let dateToAfterAddOneDay = moment(dateTo).add(86400, "s");
+      if (dateToAfterAddOneDay.isAfter(_currentMoment))
+        result = true;
     }
-
+    else
+      result = true;
+    
+    return result;
+  }
+  
+  static getCurrentStringTime(useStoreTimezone: boolean = false): string {
+    if (useStoreTimezone === true && SettingDB.getStoreConfigGroup("store")) {
+      if (SettingDB.getStoreConfigGroup("store")['time_zone'] < 0) {
+        return moment().utc().add(SettingDB.getStoreConfigGroup("store")['time_zone'], "s").format("YYYY-MM-DD HH:mm:s");
+      } else {
+        return moment().utc().subtract(SettingDB.getStoreConfigGroup("store")['time_zone'], "s").format("YYYY-MM-DD HH:mm:s");
+      }
+    } else {
+      return moment().format("YYYY-MM-DD HH:mm:s");
+    }
+  }
+  
 }
