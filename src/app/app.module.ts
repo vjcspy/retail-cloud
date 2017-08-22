8b440063f13d568b68fc2c2675d10537cf1a7317
Hung Nguyen Huy <hungnh@smartosc.com>
2017-08-22 18:29:44 +0700

    refactor code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,7 @@ import {AppStorage} from "./services/storage";
 import {LocalStorageService, SessionStorageService} from "ngx-webstorage";
 import {DialogModule} from "./modules/dialog/dialog.module";
 import {AppService} from "./app.service";
-// import {CookieService} from "angular2-cookie/core";
+import {CookieService} from "angular2-cookie/core";
 
 /**
  * `AppModule` is the main entry point into Angular2's bootstraping process
@@ -60,7 +60,7 @@ import {AppService} from "./app.service";
              * Expose our Services and Providers into Angular's dependency injection.
              */
             providers: [
-              // CookieService,
+              CookieService,
               AppService,
               {provide: ToastsManager, useClass: CustomToastOptions},
               SelectivePreloadingStrategy,
