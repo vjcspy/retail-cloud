9fc25c23f6ba05536c072372d009aa6d08d74dd8
vjcspy <mr.vjcspy@gmail.com>
2017-08-31 14:03:44 +0700

    - refactor customer other information

--- src/app/modules/+pos/view/R/sales/checkout/popup/popup.service.ts
+++ src/app/modules/+pos/view/R/sales/checkout/popup/popup.service.ts
@@ -14,15 +14,15 @@ export class CheckoutPopupService {
     _.forEach(wishlist, (item) => {
                 let canAddToQuote = false;
                 let _item         = Object.assign({}, {...item});
-                
-                if (item['buyRequest'] && item['buyRequest']['product_id']) {
-                  let product = products.find((p) => parseInt(p['id'] + '') === parseInt(item['buyRequest']['product_id']));
+      
+                if (item['buyRequest'] && item['product_id']) {
+                  let product = products.find((p) => parseInt(p['id'] + '') === parseInt(item['product_id']));
                   if (product) {
                     canAddToQuote    = true;
                     _item['product'] = product;
                   }
                 }
-                
+      
                 wishlistItems = wishlistItems.push(Object.assign(_item, {canAddToQuote}));
               }
     );
