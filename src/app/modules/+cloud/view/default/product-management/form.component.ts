fc8a54adeebbd07e5a1752728af6c4e90ea8516b
thanhnt <tatthanh.dev@gmail.com>
2017-10-26 11:45:43 +0700

    #1747: version management for pos

--- src/app/modules/+cloud/view/default/product-management/form.component.ts
+++ src/app/modules/+cloud/view/default/product-management/form.component.ts
@@ -22,11 +22,13 @@ import {Store} from "@ngrx/store";
 export class ProductFormComponent implements OnInit {
   public product = {
     pricings: [],
-    versions: []
+    versions: [],
+    apiVersions: []
   };
-  public prices  = [];
-  public data    = {};
-  
+  public prices = [];
+  public data   = {};
+  public users  = ['test@example.com', 'hihi@example.com', 'hehe@example.com']; // temporary
+
   protected validation;
   
   public productState$: Observable<ProductState>;
@@ -74,6 +76,7 @@ export class ProductFormComponent implements OnInit {
   
   private initPageJs() {
     let vm          = this;
+    this.product.apiVersions = ['1.1.0', '1.2.0', '1.2.1']; // temporary
     this.validation = jQuery('.js-validation-product')['validate']({
                                                                      errorClass: 'help-block text-right animated fadeInDown',
                                                                      errorElement: 'div',
@@ -82,13 +85,13 @@ export class ProductFormComponent implements OnInit {
                                                                      },
                                                                      highlight(e) {
                                                                        let elem = jQuery(e);
-        
+
                                                                        elem.closest('.form-group').removeClass('has-error').addClass('has-error');
                                                                        elem.closest('.help-block').remove();
                                                                      },
                                                                      success(e) {
                                                                        let elem = jQuery(e);
-        
+                                                                       
                                                                        elem.closest('.form-group').removeClass('has-error');
                                                                        elem.closest('.help-block').remove();
                                                                      },
@@ -128,31 +131,103 @@ export class ProductFormComponent implements OnInit {
                                                                    });
     
     jQuery("#val-pricings")['select2']();
+    jQuery("#modal-version-api")['select2']();
+    jQuery("#modal-version-specified-customers")['select2']();
+    
+    $(document).ready(() => {
+      $(document).on('show.bs.modal','#modal-product-versions', () => {
+        if( $('#modal-version-customers option:selected').val() === 'specified' ) {
+          $('#modal-version-specified-customers').next().css('display', 'block');
+        } else {
+          $('#modal-version-specified-customers').next().css('display', 'none');
+        }
+      });
+      
+      $('#modal-version-customers').on('change', () => {
+        if( $('#modal-version-customers option:selected').val() === 'specified' ) {
+          $('#modal-version-specified-customers').next().css('display', 'block');
+        } else {
+          $('#modal-version-specified-customers').next().css('display', 'none');
+        }
+      });
+    });
   }
-  
+
   isEditingProduct() {
     return !!this.product && !!this.product['_id'];
   }
-  
+
   goBack() {
     this.routerActions.go('cloud/default/product/list');
   }
   
   addVersion() {
-    this.product.versions.push({
-                                 name: "",
-                                 version: "",
-                                 changelog: "",
-                                 created_at: moment().toDate(),
-                                 updated_at: moment().toDate(),
-                               });
+    let versionId = $('#modal-version-id').val();
+    let customer = {
+      type: $('#modal-version-customers option:selected').val(),
+      users: []
+    };
+    if($('#modal-version-customers option:selected').val() === 'specified') {
+      customer.users = $('#modal-version-specified-customers').val();
+    }
+    
+    if( versionId == '-1' ) {
+      this.product.versions.push(
+        {
+          name: $('#modal-version-name').val(),
+          version: $('#modal-version-version').val(),
+          customers: customer,
+          api: $('#modal-version-api').val(),
+          path: $('#modal-version-path').val(),
+          descriptions: $('#modal-version-descriptions').val(),
+          created_at: moment().toDate(),
+          updated_at: moment().toDate(),
+        }
+      );
+    } else {
+      this.product.versions[versionId] = {
+        name: $('#modal-version-name').val(),
+        version: $('#modal-version-version').val(),
+        customers: customer,
+        api: $('#modal-version-api').val(),
+        path: $('#modal-version-path').val(),
+        descriptions: $('#modal-version-descriptions').val(),
+        created_at: moment().toDate(),
+        updated_at: moment().toDate(),
+      };
+    }
+  
+    this.resetModalVersion();
+  }
+  
+  editVersion(vIndex) {
+    $('#modal-version-id').val(vIndex);
+    $('#modal-version-name').val(this.product.versions[vIndex]['name']);
+    $('#modal-version-version').val(this.product.versions[vIndex]['version']);
+    $('#modal-version-customers').val(this.product.versions[vIndex]['customers']['type']);
+    $('#modal-version-specified-customers').val(this.product.versions[vIndex]['customers']['users']).trigger('change');
+    $('#modal-version-api').val(this.product.versions[vIndex]['api']).trigger('change');
+    $('#modal-version-path').val(this.product.versions[vIndex]['path']);
+    $('#modal-version-descriptions').val(this.product.versions[vIndex]['descriptions']);
   }
   
   removeVersion(version) {
     _.remove(this.product.versions, version);
   }
-  
+
   isSelectedPrice(id) {
     return _.indexOf(this.product.pricings, id) > -1;
+  }
+  
+  resetModalVersion() {
+    $('#modal-version-id').val(-1);
+    $('#modal-version-name').val('');
+    $('#modal-version-version').val('');
+    $('#modal-version-customers').val('all');
+    $('#modal-version-specified-customers').val('').trigger('change');
+    $('#modal-version-api').val('').trigger('change');
+    $('#modal-version-path').val('');
+    $('#modal-version-descriptions').val('');
+    $('.close').click();
   }
 }
