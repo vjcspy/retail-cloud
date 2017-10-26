38ad567dfa7470eb81f21109a5e9ede0d717ec6c
khoild <khoild@smartosc.com>
2017-10-26 09:03:16 +0700

    - init account license list page

--- src/app/modules/+cloud/view/default/account/license/list.component.ts
+++ src/app/modules/+cloud/view/default/account/license/list.component.ts
@@ -1,4 +1,9 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {RouterActions} from "../../../../../../R/router/router.actions";
+import {LicenseCollection} from "../../../../../../services/meteor-collections/licenses";
+import {AbstractSubscriptionComponent} from "../../../../../../code/AbstractSubscriptionComponent";
+import {Observable} from "rxjs/Observable";
+import {MongoObservable} from "meteor-rxjs";
 
 @Component({
              // moduleId: module.id,
@@ -7,8 +12,37 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
              changeDetection: ChangeDetectionStrategy.OnPush,
            })
 
-export class AccountLicenseListComponent implements OnInit {
-  constructor() { }
+export class AccountLicenseListComponent extends AbstractSubscriptionComponent implements OnInit {
+  constructor(public routerActions: RouterActions,
+              protected licenseCollection: LicenseCollection) {
+    super();
+  }
   
-  ngOnInit() { }
+  ngOnInit() {
+    this.subscribeObservable("_", () =>
+      Observable
+        .combineLatest(
+          this.licenseCollection.getCollectionObservable()
+        ).subscribe((z: any) => {
+        const licenseCollection: MongoObservable.Collection<any> = z[0];
+        
+        const license = licenseCollection.collection.findOne();
+        
+        if (license) {
+        
+        } else {
+          this.showNonelicenseInform();
+          return;
+        }
+      }));
+  }
+  
+  protected showNonelicenseInform() {
+    $('#none-license-inform')['modal']();
+  }
+  
+  goProductsPage() {
+    this.routerActions.go('cloud/default/c-product/list');
+    $('#none-license-inform')['modal']('hide');
+  }
 }
