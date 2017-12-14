a31b15bf5d079d4798d624bf354d3b8b43fedb5d
Hung Nguyen Huy <hungnh@smartosc.com>
2017-12-14 11:42:26 +0700

    view full report

--- src/app/modules/+cloud/view/dashboard/table/retail-dashboard-product-sold.ts
+++ src/app/modules/+cloud/view/dashboard/table/retail-dashboard-product-sold.ts
@@ -1,6 +1,10 @@
 import {ChangeDetectorRef, Component, Input} from '@angular/core';
 import {ReportDashboardHelper} from "../../../R/dashboard/helper";
 import * as _ from "lodash";
+import {SaleReportService} from "../../../R/report/service";
+import {Router} from "@angular/router";
+import {DashboardReportService} from "../../../R/dashboard/service";
+import * as moment from "moment";
 
 @Component({
              // moduleId: module.id,
@@ -16,14 +20,17 @@ export class RetailDashBoardTableProductSold {
   public sort_measure: string = 'revenue';
   @Input('data_view') viewData  = [];
   
-  constructor(protected changeDetector: ChangeDetectorRef) {}
+  constructor(protected changeDetector: ChangeDetectorRef,
+              protected saleReportService: SaleReportService,
+              protected reportDashBoardService: DashboardReportService,
+              private router: Router) {}
   
   getListMeasure() {
     return ReportDashboardHelper.getListMeasureProductSold();
   }
   
   sortAmount(measure) {
-    if (measure == 'revenue') {
+    if (measure === 'revenue') {
       this.sort_measure             = measure;
       this.sort_desc                = !this.sort_desc;
       this.viewData['product_sold'] = _.reverse(this.viewData['product_sold']);
@@ -33,5 +40,44 @@ export class RetailDashBoardTableProductSold {
   
   getDataProductSold() {
     return this.viewData['product_sold'];
+  }
+  
+  goToSaleReport() {
+    this.saleReportService.viewDataFilter['report_type'] = 'product';
+    this.saleReportService.viewDataFilter['dateTimeState'] = 'compare';
+    this.saleReportService.viewDataFilter['compare_value'] = this.getCompareValueSaleReport();
+    this.saleReportService.viewDataFilter['compare_type'] = 'last_from';
+    this.saleReportService.viewDataFilter['compare_count'] = 1;
+    this.initDateRangeForSaleReport();
+    
+    this.router.navigate(['/cloud/default/sale-report']);
+  }
+  
+  getCompareValueSaleReport() {
+    let period     = this.reportDashBoardService.viewDataFilter['period'];
+    let periodData = _.find(ReportDashboardHelper.getListTimePeriodPicker()['data'], (row) => row['value'] === period);
+    return periodData['for_sale_report'];
+  }
+  
+  initDateRangeForSaleReport() {
+    let compare_value = this.getCompareValueSaleReport();
+    
+    switch (compare_value) {
+      case 'day':
+        this.saleReportService.viewDataFilter['dateStart'] = moment().format("YYYY-MM-DD 00:00:00");
+        this.saleReportService.viewDataFilter['dateEnd'] = moment().format("YYYY-MM-DD 23:59:59");
+        break;
+      case 'week':
+        this.saleReportService.viewDataFilter['dateStart'] = moment().startOf('week').format("YYYY-MM-DD 00:00:00");
+        this.saleReportService.viewDataFilter['dateEnd'] = moment().endOf('week').format("YYYY-MM-DD 23:59:59");
+        break;
+      case 'month':
+        this.saleReportService.viewDataFilter['dateStart'] = moment().startOf('month').format("YYYY-MM-DD 00:00:00");
+        this.saleReportService.viewDataFilter['dateEnd'] = moment().endOf('month').format("YYYY-MM-DD 23:59:59");
+        break;
+      default:
+        break;
+    }
+    
   }
 }
