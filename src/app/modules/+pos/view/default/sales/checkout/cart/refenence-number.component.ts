47892a3eb8efe6f75a984f444fd0e38237c47e51
Hung Nguyen Huy <hungnh@smartosc.com>
2018-01-05 17:32:09 +0700

    update valid ARN

--- src/app/modules/+pos/view/default/sales/checkout/cart/refenence-number.component.ts
+++ src/app/modules/+pos/view/default/sales/checkout/cart/refenence-number.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {PosQuoteState} from "../../../../../R/quote/quote.state";
 import {PosQuoteActions} from "../../../../../R/quote/quote.actions";
+import {FormValidationService} from "../../../../../../share/provider/form-validation";
 import {NotifyManager} from "../../../../../../../services/notify-manager";
 
 @Component({
@@ -12,12 +13,23 @@ import {NotifyManager} from "../../../../../../../services/notify-manager";
 export class PosDefaultSalesCheckoutCartReferenceNumberComponent {
   @Input() posQuoteState: PosQuoteState;
   @Input('checkIsRefund') checkIsRefund: boolean;
+  protected _validProperty = {
+    isValid: true,
+    mess: ""
+  };
   
   constructor(protected posQuoteActions: PosQuoteActions,
+              protected formValidationService: FormValidationService,
               protected notify: NotifyManager) {}
   
   addReferenceNumber($event) {
-    this.posQuoteActions.addReferenceNumber($event.target['value']);
+    this._validProperty = <any>this.formValidationService.validate('applicant-reference-number', $event.target['value']);
+    if (!this._validProperty.isValid) {
+      this.notify.warning(this._validProperty.mess);
+      this.posQuoteActions.addReferenceNumber($event.target['value']);
+    } else {
+      this.posQuoteActions.addReferenceNumber($event.target['value']);
+    }
   }
   
 }
