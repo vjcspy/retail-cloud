94fab769cac4d92ab88a1c9e77aa6577a89b8cf0
khoild <khoild@smartosc.com>
2018-01-17 15:44:40 +0700

    - not allow user go to login page when logged

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -20,7 +20,7 @@ const HotModuleReplacementPlugin = require('webpack/lib/HotModuleReplacementPlug
  * Webpack Constants
  */
 const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || 'pos.cloud.local';
 const PORT = process.env.PORT || 1233;
 const PUBLIC = process.env.PUBLIC_DEV || HOST + ':' + PORT;
 const AOT = process.env.BUILD_AOT || helpers.hasNpmFlag('aot');
--- src/app/services/router-guard/index.ts
+++ src/app/services/router-guard/index.ts
@@ -1,5 +1,7 @@
 import {AuthGuard} from "./auth-guard";
+import {NotLoggedGuard} from "./not-logged-guard";
 
 export const routerGuardService = [
-  AuthGuard
+  AuthGuard,
+  NotLoggedGuard
 ];
