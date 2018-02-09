a48b64591d34851d3c696d93371f7c37d2a6e22a
khoild <khoild@smartosc.com>
2018-02-09 17:18:23 +0700

    - nothing

--- src/app/modules/+pos/pos.component.ts
+++ src/app/modules/+pos/pos.component.ts
@@ -22,6 +22,6 @@ export class PosComponent {
     this.translate.use('en');
     this.reducerManagement.replaceReducer('posReducer', posReducer());
     this.offline.init();
-    console.log('%c TLS-POS v1.0.1.201711011505 ', 'background: #222; color: #bada55');
+    console.log('%c POS v1.0.1.201802091710 ', 'background: #222; color: #bada55');
   }
 }
