a841497d2f2043e8bc6e661a023a185a262bfc0d
kid86 <thanhtd.vf@gmail.c>
2018-04-04 15:20:13 +0700

    fix issue XRT 325

--- src/app/modules/+pos/core/framework/catalog/Helper/Product.ts
+++ src/app/modules/+pos/core/framework/catalog/Helper/Product.ts
@@ -2,21 +2,26 @@ import * as _ from 'lodash';
 
 export class ProductHelper {
   protected _skipSaleableCheck = true;
-  
+
   static isSales(product: Object) {
-    return product['special_price'] != null || !_.isEmpty(product['tier_prices']);
+    let today =  Date.now();
+    if (product['special_price'] &&
+      ((today >= Date.parse( product['special_from_date']) && today <= Date.parse(product['special_to_date'])) || (today >= Date.parse( product['special_from_date']) && product['special_to_date'] === null))) {
+        return true;
+    }
+    return false;
   }
-  
+
   static isOutOfStock(product: Object) {
     return !this.isSalesAble(product);
   }
-  
+
   static isSalesAble(product: Object) {
     // FIX XRT-666 : out of stock chỉ check theo setting product chứ không check theo qty
     return !(product['stock_items'] && // apply on both Configurable/Bundle/Grouped parent product
              (parseInt(product['stock_items']['is_in_stock']) === 0));
   }
-  
+
   static getProductTaxClass(tax_class_id: number, taxClass: any): string {
     if (tax_class_id == 0) {
       return "NONE";
@@ -30,7 +35,7 @@ export class ProductHelper {
       }
     }
   }
-  
+
   getSkipSaleableCheck(): boolean {
     return this._skipSaleableCheck;
   }
